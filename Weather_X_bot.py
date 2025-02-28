fce208cb6e2ab57035fece3a3976d12f00de3843
Itz__prakash_910 <160889786+Itzprakash910@users.noreply.github.com>
2025-02-28 10:59:16 +0530

    Update Weather_X_bot.py

--- Weather_X_bot.py
+++ Weather_X_bot.py
@@ -1,4 +1,4 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton,  InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 import requests
 
